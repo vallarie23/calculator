f27c5c124a739f1826a37358e1baa13d56c5293f
vallarie23 <vallaryotieno480@gmail.com>
2023-03-20 12:27:33 +0300

    my first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let display =document.getElementById('display');
+
+let button = Array.from(document.getElementsByClassName('button'));
+
+button.map( button => {
+    button.addEventListener('click', (e) => {
+
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText ='';
+                break;
+                case '<-':
+                    if(display.innerText){
+                        display.innerText =display.innerText.slice(0,-1);
+                    }
+                    break;
+                    case '=':  
+                    try{
+                        display.innerText =eval(display.innerText);
+                    }catch{
+                        display.innerText ='Error!';
+                    }
+                    break;
+            default:
+                display.innerText <= e.target.innerText;
+                
+        }
+    })
+}); 
+
